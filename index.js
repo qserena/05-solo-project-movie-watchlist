d9b3c8f70d0ad705a27689617139867789c8bf93
Tom Serenander <tom@serenander.se>
2023-10-25 11:02:37 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const apiKey = `7575777d`
+const searchInput = document.getElementById('search-input')
+const searchBtn = document.getElementById('search-btn')
+const form = document.getElementById('form')
+const movieList = document.getElementById('movie-list')
+
+let movies = []
+
+form.addEventListener('submit', function (e) {
+	e.preventDefault()
+
+	if (searchInput.value) {
+		fetch(
+			`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchInput.value}`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data)
+				movies = data.Search
+				renderMovies()
+			})
+	}
+})
+
+function renderMovies() {
+	if (movies.length > 0) {
+		let html = ``
+		for (let movie of movies) {
+			html += `
+                <li>${movie.Title}</li>
+            `
+		}
+		movieList.innerHTML = html
+	}
+}
