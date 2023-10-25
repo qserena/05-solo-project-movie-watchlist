57e2bb370daad6d419c8bb12ba51eb1f0009a3ac
Tom Serenander <tom@serenander.se>
2023-10-25 16:35:16 +0200

    Basic functionality - no styling.

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ const searchBtn = document.getElementById('search-btn')
 const form = document.getElementById('form')
 const movieList = document.getElementById('movie-list')
 
-let movies = []
+let searchedMovieList = []
+
+function getWatchListIds() {
+	return JSON.parse(localStorage.getItem('watch-list-ids'))
+}
+
+document.addEventListener('click', function (e) {
+	if (e.target.dataset.imdbId) {
+		handleAddClick(e.target.dataset.imdbId)
+	}
+})
+
+function handleAddClick(imdbId) {
+	let watchListIds = getWatchListIds() || []
+	console.log(watchListIds)
+	watchListIds.push(imdbId)
+	localStorage.setItem('watch-list-ids', JSON.stringify(watchListIds))
+}
 
 form.addEventListener('submit', function (e) {
 	e.preventDefault()
@@ -15,22 +32,22 @@ form.addEventListener('submit', function (e) {
 		)
 			.then((res) => res.json())
 			.then((data) => {
-				console.log(data)
-				movies = data.Search
+				searchedMovieList = data.Search
 				renderMovies()
 			})
 	}
 })
 
 function renderMovies() {
-	if (movies.length > 0) {
+	if (searchedMovieList.length > 0) {
 		let html = ``
-		for (let movie of movies) {
+		for (let movie of searchedMovieList) {
 			html += `
                 <li>
-                    ${movie.Title} 
-                    <div class="add-btn align-right" data-item-id=${movie.id}>
-                        <p data-item-id=${movie.id}>Watchlist</p>
+                    <p>${movie.Title}</p>
+                    <div class="add-btn align-right">
+                        <img class="icon" src="./img/add-icon-large.png" data-imdb-id="${movie.imdbID}"/>
+                        <p data-imdb-id="${movie.imdbID}">Watchlist</p>
                     </div>
                 </li>
             `
---
+++ watchlist.js
@@ -0,0 +1,39 @@
+const moviesList = document.getElementById('movies-list')
+
+document.addEventListener('click', function (e) {
+	if (e.target.dataset.imdbId) {
+		handleRemoveClick(e.target.dataset.imdbId)
+	}
+})
+
+function handleRemoveClick(imdbId) {
+	let watchListIds = getWatchListIds()
+	watchListIds = watchListIds.filter((i) => i !== imdbId)
+	localStorage.setItem('watch-list-ids', JSON.stringify(watchListIds))
+	renderMovies()
+}
+
+function getWatchListIds() {
+	return JSON.parse(localStorage.getItem('watch-list-ids'))
+}
+
+function renderMovies() {
+	const movieIds = getWatchListIds()
+
+	let html = ''
+	for (let id of movieIds) {
+		html += `
+            <li>
+            <p>${id}</p>
+            <div class="remove-btn align-right">
+                <img class="icon" src="./img/remove-icon-large.png" data-imdb-id="${id}"/>
+                <p data-imdb-id="${id}">Remove</p>
+            </div>
+            </li>
+        `
+	}
+
+	moviesList.innerHTML = html
+}
+
+renderMovies()
